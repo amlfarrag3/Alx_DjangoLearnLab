a74ddd4a9f510e0e39201bf0d661efed44934ea3
amlfarrag3 <farragaml983@gmail.com>
2025-07-21 00:22:47 +0300

    Add files via upload

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,30 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+author_name = "J.K. Rowling"
+try:
+    author = Author.objects.get(name=author_name)
+    books_by_author = author.books.all()
+    print(f"Books by {author.name}:")
+    for book in books_by_author:
+        print(f"- {book.title}")
+except Author.DoesNotExist:
+    print(f"No author found with name '{author_name}'.")
+
+# List all books in a library
+library_name = "Central Library"
+try:
+    library = Library.objects.get(name=library_name)
+    books_in_library = library.books.all()
+    print(f"\nBooks in {library.name}:")
+    for book in books_in_library:
+        print(f"- {book.title}")
+except Library.DoesNotExist:
+    print(f"No library found with name '{library_name}'.")
+
+# Retrieve the librarian for a library
+try:
+    librarian = library.librarian  # uses related_name='librarian'
+    print(f"\nLibrarian of {library.name}: {librarian.name}")
+except Librarian.DoesNotExist:
+    print(f"No librarian assigned to {library.name}")
---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register_view, list_books, LibraryDetailView
+from .views import admin_view, librarian_view, member_view
+from .views import add_book, edit_book, delete_book
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('register/', register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('admin-panel/', admin_view, name='admin_view'),
+    path('librarian-panel/', librarian_view, name='librarian_view'),
+    path('member-panel/', member_view, name='member_view'),
+        path('books/add/', add_book, name='add_book'),
+    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', delete_book, name='delete_book')
+    
+]
