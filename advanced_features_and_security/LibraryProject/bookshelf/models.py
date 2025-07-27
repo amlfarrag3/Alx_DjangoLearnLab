319c9c565a2343a56f9f9357c54aec278ff3db58
amlfarrag3 <farragaml983@gmail.com>
2025-07-28 00:31:14 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -49,5 +49,13 @@ class Book(models.Model):
     publication_year = models.IntegerField()
     user = models.OneToOneField(settings.AUTH_USER_MODEL,null=True, on_delete=models.CASCADE)
 
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
     def __str__(self):
-      return self.title
+        return self.title
