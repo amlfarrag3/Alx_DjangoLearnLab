25daf92ff8f23f8fa23a63d44608d70d12e926ad
amlfarrag3 <farragaml983@gmail.com>
2025-07-28 00:47:22 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,22 @@
-# bookshelf/forms.py
-
 from django import forms
 from .models import Book
+from django import forms
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        label='Your Name',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        label='Email Address',
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    message = forms.CharField(
+        label='Message',
+        widget=forms.Textarea(attrs={'class': 'form-control'})
+    )
+
 
 class BookForm(forms.ModelForm):
     class Meta:
