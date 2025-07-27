a2fdda9a4212ebba33d256884015e2e4b544efee
amlfarrag3 <farragaml983@gmail.com>
2025-07-28 00:43:19 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -12,3 +12,10 @@ def edit_book(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     # Handle form/edit logic here
     return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+
+def search_books(request):
+    query = request.GET.get('q', '')
+    books = Book.objects.filter(title__icontains=query)
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
