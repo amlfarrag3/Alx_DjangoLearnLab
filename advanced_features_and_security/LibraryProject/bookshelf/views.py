27d9ccd2a96e74d578cf8c5529c4426f31510b05
amlfarrag3 <farragaml983@gmail.com>
2025-07-28 00:49:20 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from .models import Book
+from .forms import ExampleForm
+
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
@@ -19,3 +21,9 @@ def search_books(request):
     books = Book.objects.filter(title__icontains=query)
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
+def example_form_view(request):
+    form = ExampleForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        # process form.cleaned_data here
+        return render(request, 'bookshelf/form_success.html')
+    return render(request, 'bookshelf/form_example.html', {'form': form})
