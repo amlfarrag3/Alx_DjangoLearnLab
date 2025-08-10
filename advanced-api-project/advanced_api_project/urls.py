0ad5d4312393a4f304925c76d34741c0059a2141
amlfarrag3 <farragaml983@gmail.com>
2025-08-10 20:30:07 +0300

    Update urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -19,12 +19,11 @@ from django.urls import path, include
 from rest_framework import routers
 from api.views import AuthorViewSet, BookViewSet
 
-router = routers.DefaultRouter()
-router.register(r'authors', AuthorViewSet)
-router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include('api.urls')),
 ]
+
+
 
